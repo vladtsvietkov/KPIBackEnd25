8d2c79d3abca3bc89a8bd01df14c6ad9cc59f36c
vladtsvietkov <dlyaservisovai@gmail.com>
2025-12-12 17:13:00 +0200

    update init to handle jwt errors

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,6 +1,6 @@
-from flask import Flask
+from flask import Flask, jsonify
 from backend.config import Config
-from backend.extensions import db, migrate, api
+from backend.extensions import db, migrate, api, jwt
 from backend import views
 
 
@@ -13,5 +13,29 @@ def create_app():
     api.init_app(app)
 
     api.register_blueprint(views.blp)
+
+    @jwt.expired_token_loader
+    def expired_token_callback(jwt_header, jwt_payload):
+        return (
+            jsonify({"message": "The token has expired.", "error": "token_expired"}),
+            401,
+        )
+
+    @jwt.invalid_token_loader
+    def invalid_token_callback(error):
+        return (
+            jsonify({"message": "Signature verification failed.", "error": "invalid_token"}),
+            401,
+        )
+
+    @jwt.unauthorized_loader
+    def missing_token_callback(error):
+        return (
+            jsonify({
+                "description": "Request does not contain an access token.",
+                "error": "authorization_required",
+            }),
+            401,
+        )
 
     return app
